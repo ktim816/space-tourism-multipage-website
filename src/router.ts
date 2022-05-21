86f96ea830a904d044f4bd35c83dc343ef3c2399
Timur Kostenko <ktim8168@mail.ru>
2022-05-21 20:59:40 +0300

    updates

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,7 @@ import DestinationView from '@views/DestinationView.vue';
 import CrewView from '@views/CrewView.vue';
 import TechnologyView from '@views/TechnologyView.vue';
 
-const history = createWebHistory();
+const history = createWebHistory('/space-tourism-multipage-website/');
 const routes: RouterOptions['routes'] = [
   {
     path: '/',
